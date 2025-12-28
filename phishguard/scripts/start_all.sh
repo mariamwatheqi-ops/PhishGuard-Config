ea7130a20026e9d1ad766bf751ccd7db9aebc7d9
mariamwatheqi-ops <mariam.watheqi@gmail.com>
2025-12-28 20:51:31 +0300

    Delete phishguard directory

--- phishguard/scripts/start_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /home/ubuntu/phishguard/venv/bin/activate
-
-# Run flask_app.py which includes both dashboard and ingestion logic
-exec gunicorn --bind 127.0.0.1:8000 flask_app:app
-
